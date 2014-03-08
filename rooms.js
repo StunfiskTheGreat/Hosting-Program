e79bbaf9490abf4f06f4533d2505aae6ef455838
BlakJack <BlakJack@users.noreply.github.com>
2014-03-08 19:20:32 +0530

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1397,7 +1397,6 @@ var ChatRoom = (function() {
 			Tournaments.get(this.id).update(user);
 		if (this.reminders && this.reminders.length > 0)
 			CommandParser.parse('/reminder', this, user, connection);
-		CommandParser.parse('/donate', this, user, connection);
 	};
 	ChatRoom.prototype.onJoin = function(user, connection, merging) {
 		if (!user) return false; // ???
@@ -1423,7 +1422,6 @@ var ChatRoom = (function() {
 			this.send('|init|chat\n|title|'+this.title+'\n'+userList+'\n'+this.logGetLast(100).join('\n')+modchat, connection);
 			if (this.reminders && this.reminders.length > 0)
 				CommandParser.parse('/reminder', this, user, connection);
-			CommandParser.parse('/donate', this, user, connection);
 		}
 		if (global.Tournaments && Tournaments.get(this.id))
 			Tournaments.get(this.id).update(user);
